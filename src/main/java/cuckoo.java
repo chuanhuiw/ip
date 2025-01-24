ae26476aef7d46c9921d2e74ca09b5c4c808f32f
chuanhuiw <chuanhui.wong@gmail.com>
2025-01-24 17:16:51 +0800

    Add Echo feature

--- src/main/java/cuckoo.java
+++ src/main/java/cuckoo.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class cuckoo {
     public static void main(String[] args) {
         String logo = "cuckoo";
-        System.out.println("Hello! I'm " + logo);
+        System.out.println("ANYEONG! I am " + logo);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("______________________");
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!line.equals("bye")) {
+            System.out.println(line);
+            System.out.println("______________________");
+            line = in.nextLine();
+        }
+        System.out.println("Bye.");
     }
 }
