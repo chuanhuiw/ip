c980e709530c581bba8abae21c8ab1c51b2029ab
chuanhuiw <chuanhui.wong@gmail.com>
2025-01-24 15:46:03 +0800

    Change chatbot name

---
+++ src/main/java/cuckoo.java
@@ -0,0 +1,8 @@
+public class cuckoo {
+    public static void main(String[] args) {
+        String logo = "cuckoo";
+        System.out.println("Hello! I'm " + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
