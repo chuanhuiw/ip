5e9da908448483bb9a604ef11d2ef5d597e9bc9e
chuanhuiw <chuanhui.wong@gmail.com>
2025-02-02 00:33:22 +0800

    Add "Add, List" feature

--- src/main/java/cuckoo.java
+++ src/main/java/cuckoo.java
@@ -2,18 +2,39 @@ import java.util.Scanner;
 
 public class cuckoo {
     public static void main(String[] args) {
-        String logo = "cuckoo";
-        System.out.println("ANYEONG! I am " + logo);
+        String chatbotName = "cuckoo";
+        System.out.println("ANNYEONG! I am " + chatbotName);
         System.out.println("What can I do for you?");
-        System.out.println("______________________");
-        String line;
+        System.out.println("____________________________________________________________");
+        String[] tasks = new String[100];
+        int count = 0;
         Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        while(!line.equals("bye")) {
-            System.out.println(line);
-            System.out.println("______________________");
-            line = in.nextLine();
+        while (true) {
+            String input = in.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < count; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                if (count < 100) {
+                    tasks[count++] = input;
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" added: " + input);
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println(" Task list is full!");
+                }
+            }
         }
-        System.out.println("Bye.");
+
+        in.close();
     }
 }
